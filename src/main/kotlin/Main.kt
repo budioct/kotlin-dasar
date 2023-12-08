7cb28b9a509b9bed5a29416efa35255cdabe7003
Budhi Octaviansyah <budioct@gmail.com>
2023-12-08 23:55:20 +0700

    start project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    // fun keyword untuk membuat function
+    // main() adalah main function di kotlin
+    // di kotlin tidak wajib bikin class
+    // di kotlin tidak wajib menggunakan titik koma ';'
+
+    println("Hello World")
+}
