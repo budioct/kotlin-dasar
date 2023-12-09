eafdd815a4567072232624dae85257bbe43ecfb1
Budhi Octaviansyah <budioct@gmail.com>
2023-12-09 08:08:02 +0700

    while loops

---
+++ src/main/kotlin/WhileLoops.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    whileLoops()
+
+}
+
+fun whileLoops() {
+
+    var i: Int = 0
+
+    var totalNilai = 0
+    while (i < 10) {
+        i++
+        totalNilai += i
+        println("loops $i")
+    }
+    println("Total Nilai $totalNilai")
+    println("Selesai Looping")
+
+}
