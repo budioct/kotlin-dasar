727da95a6bc2834dc2bec392af307e45a113c995
Budhi Octaviansyah <budioct@gmail.com>
2023-12-09 07:10:56 +0700

    comparison

---
+++ src/main/kotlin/Comparison.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    compare()
+
+}
+
+fun compare(){
+
+    val a = 1000
+    val b = 2000
+    val result: Boolean = a <= b
+
+    println(result)
+    println(a <= b)
+
+    println(1000 <= 2000)
+    println(1000 >= 2000)
+
+    println("a" < "b")
+    println("a" != "b")
+
+}
