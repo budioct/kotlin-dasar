6744b50677d3f5bb191e999a9c7e432b96f99c25
Budhi Octaviansyah <budioct@gmail.com>
2023-12-09 13:35:08 +0700

    function argument

---
+++ src/main/kotlin/FunctionParam.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    sayHello("Bambang", "pamungkas")
+    sayHello("Mo", "salah")
+    sayHello("Riski", "ridho")
+
+}
+
+fun sayHello(firstName: String, lastName: String){
+
+    println("my name is $firstName $lastName")
+}
