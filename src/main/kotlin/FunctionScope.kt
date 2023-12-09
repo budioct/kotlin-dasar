7b67310b22e49d56058b45e16ff98afd272dc6c5
Budhi Octaviansyah <budioct@gmail.com>
2023-12-09 15:42:45 +0700

    function scope

---
+++ src/main/kotlin/FunctionScope.kt
@@ -0,0 +1,24 @@
+fun main() {
+    // function inner child
+    fun helloWorldAble() {
+        println("helloWorldAble()")
+    }
+
+    // access able
+    helloWorldAble()
+}
+
+
+fun sun() {
+//    error access, scope
+//    helloWorldAble()
+}
+
+fun sum() {
+//    error access, scope
+//    helloWorldAble()
+}
+
+
+
+
