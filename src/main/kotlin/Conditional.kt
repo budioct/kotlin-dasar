a64c40322881ba2dc519bfab1180dfea517dfbb7
Budhi Octaviansyah <budioct@gmail.com>
2023-12-09 07:27:18 +0700

    conditional

---
+++ src/main/kotlin/Conditional.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    percabangan()
+
+}
+
+fun percabangan() {
+
+    val examValue: Int = 80
+
+    if (examValue >= 80) {
+        println("Good Job")
+    } else if (examValue >= 75) {
+        println("Not Bad")
+    } else {
+        println("try again next month")
+    }
+
+}
