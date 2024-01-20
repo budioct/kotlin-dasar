2b0c7575242e0eb540840f488e90841bc921c322
Budhi Octaviansyah <budioct@gmail.com>
2024-01-20 12:37:27 +0700

    update note

--- src/main/kotlin/HigherOrderFunction.kt
+++ src/main/kotlin/HigherOrderFunction.kt
@@ -4,20 +4,25 @@ fun main() {
      * Higher-Order Function adalah function yang menggunakan function sebagai parameter atau return type function
      */
 
+    // fun namefunction(param1, lambda_function)
     // transformer: (String) -> String // create Higher-Order Function, kalau mau buat harus di paling akhir parameter
     fun hello(name: String, transformer: (String) -> String): String {
         val nameTransform = transformer(name)
         return "Hello $nameTransform"
     }
 
+//    val lambdaUpper : (String) -> String = { value: String -> value.toUpperCase() }
     val lambdaUpper = { value: String -> value.toUpperCase() }
     println(hello("budhi", lambdaUpper))
 
-    println(hello("husein", { value: String -> value.toLowerCase() })) // akses langsung di method
+    println(hello("husein", { value: String -> value.toLowerCase() })) // akses lamda langsung di method
 
-    // trailing lambda (optional)
     val result1 = hello("adam", { value: String -> value.toUpperCase() })
-    val result2 = hello("jamal") { value: String -> value.toLowerCase() }
+
+    // trailing lambda (optional) // lebih mudah di baca dibanding baris 18
+    val result2 = hello("jamal") { value: String ->
+        value.toLowerCase()
+    }
 
     println(result1)
     println(result2)
--- src/main/kotlin/LambdaExpression.kt
+++ src/main/kotlin/LambdaExpression.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    // var data = (String param) -> {param.toUpperCase()}
+    // var data = (String param) type lambda -> String return value function {firstName: String deklarasi param -> block body manipulation/modified}
 
     // lambda 1
     val lambdaName1: (String) -> String = { value: String ->
@@ -12,13 +12,13 @@ fun main() {
 
     // lambda 2..
     val lambdaName2: (String) -> String = {
-        it.toUpperCase() // keyword it menganti variable yang tidak di deklarasi
+        "Hello " + it.toUpperCase() // keyword it menganti variable yang tidak di deklarasi.. bisa jalan kalau parameter nya 1 dan digunakan di body lambda saja
     }
     val name2 = lambdaName2("husein")
     println(name2)
 
     // lambda 3.. method reference
-    val lambdaName3: (String) -> String = ::toUpper
+    val lambdaName3: (String) -> String = ::toUpper // :: object reference
     val name3 = lambdaName3("adam")
     println(name3)
 
